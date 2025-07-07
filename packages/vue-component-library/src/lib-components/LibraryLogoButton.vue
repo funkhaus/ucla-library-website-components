b9e28d54e90603d68c7f5594bd86d80fc9a90c62
Austin Blanchard <austinpdx@gmail.com>
2025-07-07 09:46:44 -0700

    fixed props

--- packages/vue-component-library/src/lib-components/LibraryLogoButton.vue
+++ packages/vue-component-library/src/lib-components/LibraryLogoButton.vue
@@ -1,24 +1,15 @@
 <script setup>
-import { computed } from 'vue'
-
 // Components
-import LogoLibrary from 'ucla-library-design-tokens/assets/svgs/logo-library.svg'
-import SmartLink from '@/lib-components/SmartLink.vue'
+import LogoLibrary from "ucla-library-design-tokens/assets/svgs/logo-library.svg"
+import SmartLink from "@/lib-components/SmartLink.vue"
 
-// Props
 const props = defineProps({
-  to: {
-    type: String,
-    default: ''
-  },
+    to: String,
 })
 </script>
 
 <template>
-    <smart-link
-      class="library-logo-button"
-      :to="props.to"
-    >
-      <LogoLibrary />
+    <smart-link class="library-logo-button" :to="props.to">
+        <LogoLibrary />
     </smart-link>
 </template>
