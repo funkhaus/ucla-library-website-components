ef6cf0b6a92266e821eff04f0aae96e29a3b39ac
Luka Vuk <luka.vuk0909@gmail.com>
2025-08-07 12:22:57 +0200

    add masonry layout

--- packages/vue-component-library/src/lib-components/GridAssetPod.vue
+++ packages/vue-component-library/src/lib-components/GridAssetPod.vue
@@ -1,37 +1,78 @@
 <script setup lang="ts">
 // Imports
-import { computed } from "vue"
+import { computed, nextTick, ref, useTemplateRef, watch } from "vue"
 import BlockAssetPod from "@/lib-components/BlockAssetPod.vue"
+import GridMasonry from "@/lib-components/GridMasonry.vue"
 import type { BlockAssetPodProps } from "@/types/components/blockAssetPods.types"
+import delay from "@/utils/delay"
 
 interface GridAssetPodProps {
     readonly items: readonly BlockAssetPodProps[]
+    readonly isGridLayout?: boolean
 }
 
 // Props
 const props = defineProps<GridAssetPodProps>()
 
+// Data
+const refGridMasonry = useTemplateRef("refGridMasonry")
+
 // Computeds
+const classes = computed(() => [
+    "grid-asset-pod",
+    "dlc",
+    props.isGridLayout ? "is-grid-layout" : "is-list-layout",
+])
+
 const hasItems = computed(() => props.items.length > 0)
+
+// Methods
+const isLastItem = (index: number) => {
+    return index === props.items.length - 1
+}
+
+// Watchers
+watch(
+    () => props.isGridLayout,
+    async () => {
+        await nextTick()
+        await delay(100)
+        // Call refresh method on the GridMasonry component
+        refGridMasonry.value?.refresh?.()
+    }
+)
 </script>
 
 <template>
     <div
         v-if="hasItems"
-        class="grid-asset-pod dlc"
+        :class="classes"
         :aria-label="`Asset grid with ${items.length} items`"
     >
-        <div class="block-container" v-for="item in items" :key="item.title">
-            <BlockAssetPod
-                :title="item.title"
-                :to="item.to"
-                :date="item.date"
-                :description="item.description"
-                :resource-type="item.resourceType"
-                :collection="item.collection"
-                :image="item.image"
-            />
-        </div>
+        <GridMasonry
+            ref="refGridMasonry"
+            :transition-duration="1000"
+            stagger="0"
+            class="grid-masonry"
+            item-selector=".block-container"
+        >
+            <div
+                class="block-container"
+                v-for="(item, index) in items"
+                :key="item.title"
+            >
+                <BlockAssetPod
+                    :title="item.title"
+                    :to="item.to"
+                    :date="item.date"
+                    :description="item.description"
+                    :resource-type="item.resourceType"
+                    :collection="item.collection"
+                    :image="item.image"
+                />
+                <div class="divider" v-if="!isLastItem(index)" />
+            </div>
+        </GridMasonry>
     </div>
 </template>
 
